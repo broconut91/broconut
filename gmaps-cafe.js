c67154489e3e9bc2b9060b847bcd1cd24fc1d5b9
diegorbarca <35038549+diegorbarca@users.noreply.github.com>
2018-01-04 15:12:22 -0300

    Update gmaps-cafe.js

--- gmaps-cafe.js
+++ gmaps-cafe.js
@@ -1,8 +1,10 @@
     var locations = [
+      ['Telmo Deli <p><b>Descuento 15%</b></p> <p>Chacabuco 752 </p>',-34.6168133,-58.3758533, 5]  
       ['Good Coffee <p><b>Descuento 35%</b></p> <p>Ciudad de la Paz 1898</p>', -34.564625,-58.4565646, 4],
       ['Origen Cafe<p><b>Descuento 10%</b></p> <p>Humberto Primo 599</p>', -34.620679,-58.3752348, 3],
       ['Santal <p><b>Descuento 10%</b></p> <p> Virrey del Pino 2235</p>', -34.5652639,-58.4527318, 2],
       ['La Panotteca <p><b>Descuento 15%</b></p> <p> Zapiola 2115</p>', -34.5658051,-58.4656836, 1],
+        
        
     ];
 
