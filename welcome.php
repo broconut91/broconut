853c9bfc638e7182f9aac6f3b51ee01277588bb0
diegorbarca <35038549+diegorbarca@users.noreply.github.com>
2018-02-03 12:22:45 -0300

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+        <p class="title">Gracias por tu reserva <?php echo $_GET["name"]; ?> !</p><br>
+
+<?php mail("diego@co-work.com.ar","RESERVA",$_GET["email"];),$_GET["name"];); ?>
+
+</body>
+</html>
